e4641de76723bd9c6339e104ddeb954178f3233e
longsslz <1632296353@qq.com>
2024-10-03 22:38:33 +0800

    10.3 update 77

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,13 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // 基本情况：如果 n == 0 或 1，返回 1
+    if num == 0 || num == 1 {
+        1
+    } else {
+        // 递归调用：n * factorial(n - 1)
+        num * factorial(num - 1)
+    }
 }
 
 #[cfg(test)]
